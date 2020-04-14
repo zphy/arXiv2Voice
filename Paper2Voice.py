22460ebfbefceee5114b69113f285408a14a935c
zphy <zphy@users.noreply.github.com>
2020-04-13 22:25:14 -0400

    Fixed issues with arXiv retrieval and citation reading
    1. Some old arXiv papers only have a single file, and are thus .gz instead of .tar.gz, which caused the decompression to be incorrect. This is fixed now by checking the success of the .tar.gz extraction.
    2. There were also issues where the citations were being read out, breaking the flow of the paper (e.g. 2004.01746); this was in conflict with the way brackets were being matched for e.g. 1810.03787, which would cause citations to gobble up actual text, and more seriously leave imbalanced $ symbols for math mode. We have now fixed this in line 118-120 by using non-greedy matching.
    3. Fixed default rate format to be consistent with the input arguments.
    4. Fixed issues with \input compatibility, now works for \input{file}, \input{file.tex} and \input{file.bbl}

--- Paper2Voice.py
+++ Paper2Voice.py
@@ -33,13 +33,14 @@ Usage example: "python Paper2Voice.py main.tex"
 import re, os, sys, tarfile, shutil, getopt
 from striprtf.striprtf import rtf_to_text
 import urllib.request as request
+import gzip
 
 # latex version
 arxiv_id = str(sys.argv[1]) # zeroth argument is the current filename
 #fn = str(sys.argv[1][:-4])
 
 # default options
-rate = 200
+rate = '200'
 
 # get options
 try:
@@ -61,16 +62,25 @@ print('Loading files from arXiv:'+arxiv_id)
 url = 'https://arxiv.org/e-print/'+arxiv_id
 arxiv_id = arxiv_id.split('/')[-1] # only include last number
 print(os.getcwd()+'/'+arxiv_id+'.tar.gz')
-filename, headers = request.urlretrieve(url, os.getcwd()+'/'+arxiv_id+'.tar.gz')
+request.urlretrieve(url, os.getcwd()+'/'+arxiv_id+'.tar.gz')
 
 # extract files
+filetype = 0
 try:
     tar = tarfile.open(arxiv_id+'.tar.gz')
     tar.extractall(arxiv_id)
     tar.close()
-except: # not tar.gz format, try .tex
+except: # not tar.gz format, likely due to it being a single file, try .gz
+    filetype = 1
+    os.rename(arxiv_id+'.tar.gz',arxiv_id+'.gz')
+    f = gzip.open(arxiv_id+'.gz', 'rb')
+    file_content = f.read().decode("utf-8")
+    file_content = file_content[file_content.find('documentclass')-1:]
+    f.close()
     os.mkdir(arxiv_id)
-    os.rename(arxiv_id+'.tar.gz',arxiv_id+'/main.tex')
+    with open(arxiv_id+'/main.tex','w') as f:
+        f.write(file_content)
+    
 
 # extract .tex file
 fn_list = [f for f in os.listdir(arxiv_id+'/') if f[-4:]=='.tex']
@@ -98,13 +108,16 @@ with open(fn+'.tex','r+') as f:
     inputs = re.findall("\\\\input\{.+\}",text)
     #print(inputs)
     for i in range(len(inputs)):
-        #inputs[i] = inputs[i].replace('.tex','')
-        with open(arxiv_id+'/'+inputs[i][7:-1],'r') as f1:
+        if not (re.search('bbl',inputs[i]) or re.search('tex',inputs[i])):  # make format uniform
+            fninput = arxiv_id+'/'+inputs[i][7:-1]+'.tex'
+        else:
+            fninput = arxiv_id+'/'+inputs[i][7:-1]
+        with open(fninput,'r') as f1:
             text1 = ''.join(f1.readlines())
             text = text.replace(inputs[i],text1)
-    text = re.sub(r"\\citet\{.+\}","",text)
-    text = re.sub(r"\\citep\{.+\}","",text)
-    #text = re.sub(r"\\cite\{.+\}","",text)
+    text = re.sub(r"\\citet\{.+?\}","",text)
+    text = re.sub(r"\\citep\{.+?\}","",text)
+    text = re.sub(r"\\cite\{.+?\}","",text)
     # couldn't find a systematic fix for this easily
     text = text.replace("\\newcommand\\AND{\n    \\end{tabular}\\hfil\\linebreak[4]\\hfil\n    \\begin{tabular}[t]{c}\\ignorespaces\n}","")
 with open(fn+'.tex','w') as f:
@@ -115,7 +128,7 @@ with open(fn+'.rtf','r') as f:
     text = rtf_to_text(' '.join(text))
     #print(text)
     text.replace('\n','').replace('\t','').replace('\xa0','')
-    text = re.sub(r'\[.*\]','',text)
+    #text = re.sub(r'\[.*\]','',text)    # these are typically citations, but we have removed all citations now
     ind = text.find('noop')
     text = text[0:ind]
     ind = text.find('ostop')
@@ -129,5 +142,8 @@ mp3make='/usr/local/bin/lame -h '+fn+'.aiff '+fn+'.mp3'
 os.system(aiffmake)
 os.system(mp3make)
 os.rename(fn+'.mp3','output/'+arxiv_id+'.mp3')
-os.remove(arxiv_id+'.tar.gz')
+if filetype:
+    os.remove(arxiv_id+'.gz')
+else:
+    os.remove(arxiv_id+'.tar.gz')
 shutil.rmtree(arxiv_id+'/')
